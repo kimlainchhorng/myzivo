a1e19bfb6efc976bb683b5443028981d92154002
kimlainchhorng <abigailcompletecarcare@gmail.com>
2026-03-08 20:26:57 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,49 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.72f993409c9f453aacff60e5a9b25774',
+  appId: 'com.zivo.app',
   appName: 'myzivo',
   webDir: 'dist',
-  server: {
-    url: 'https://72f99340-9c9f-453a-acff-60e5a9b25774.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    PushNotifications: {
-      presentationOptions: ['badge', 'sound', 'alert']
-    },
-    SplashScreen: {
-      launchAutoHide: true,
-      launchShowDuration: 2000,
-      backgroundColor: '#0D0D0F',
-      showSpinner: false,
-      androidScaleType: 'CENTER_CROP',
-      splashFullScreen: true,
-      splashImmersive: true
-    },
-    StatusBar: {
-      style: 'DARK',
-      backgroundColor: '#0D0D0F'
-    },
-    Keyboard: {
-      resize: 'body',
-      resizeOnFullScreen: true
-    },
-    Haptics: {}
-  },
-  ios: {
-    backgroundColor: '#0D0D0F',
-    contentInset: 'automatic',
-    preferredContentMode: 'mobile',
-    scrollEnabled: true
-  },
-  android: {
-    backgroundColor: '#0D0D0F',
-    allowMixedContent: true,
-    captureInput: true,
-    webContentsDebuggingEnabled: false,
-    adjustMarginsForEdgeToEdge: 'auto'
-  }
 };
 
 export default config;
